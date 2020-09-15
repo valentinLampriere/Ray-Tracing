4e3e7a2d50d3ed575603b5d9b8739520967fa1ad
Valentin Lamprière <valentin.lampriere1@gmail.com>
2020-09-15 10:52:41 +0200

    statr implementing  hit_sphere function

--- intersection-rayon-sphere.cpp
+++ intersection-rayon-sphere.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include "lib/vector.cpp"
+#include "lib/lodepng.cpp"
+
+void hit_sphere(Vector3 centerSphere, float radiusSphere, Vector3 startRay, Vector3 directionRay) {
+	Vector3 oc = startRay - centerSphere;
+	float a = Vector3::dot(directionRay, directionRay);
+	float b = 2.0 * Vector3::dot(oc, directionRay);
+	float c = Vector3::dot(oc, oc) - radiusSphere * radiusSphere;
+	float discriminant = b * b - 4 * a * c;
+}
 
 int main() {
-
 }
