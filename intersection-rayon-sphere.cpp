4c41871f0b290c6dce40158ef1060954219d4d1e
Valentin Lamprière <valentin.lampriere1@gmail.com>
2020-09-15 14:01:08 +0200

    Detect sphere + vector workaround

--- intersection-rayon-sphere.cpp
+++ intersection-rayon-sphere.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "lib/vector.cpp"
-#include "lib/lodepng.cpp"
+#include "lib/vector.h"
+#include "lib/lodepng.h"
 
 float hit_sphere(Vector3 centerSphere, float radiusSphere, Vector3 startRay, Vector3 directionRay) {
 	Vector3 oc = startRay - centerSphere;
@@ -11,7 +11,7 @@ float hit_sphere(Vector3 centerSphere, float radiusSphere, Vector3 startRay, Vec
 	if (discriminant < 0)
 		return -1; // Use proper way to return bad value ( std::optional )
 	else 
-		return(-b - std::sqrt(discriminant)) / (2.0 * a);
+		return(-b - sqrt(discriminant)) / (2.0 * a);
 }
 
 void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsigned width, unsigned height) {
@@ -22,10 +22,18 @@ void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsi
 	if (error) std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 }
 
+void setColor(std::vector<unsigned char> image, int index, int r, int g, int b, int alpha) {
+	image[index] = r;			// Red
+	image[index + 1] = g;		// Green
+	image[index + 2] = b;		// Blue
+	image[index + 3] = alpha;	// Alpha
+}
+
 int main() {
 
-	Vector3 sphereCenter = Vector3(125, 125, 100);
-	float	sphereRadius = 50;
+	Vector3 sphereCenter = Vector3(125, 125, 50);
+	Vector3 lightPosition = Vector3(80, 200, 80);
+	float	sphereRadius = 25;
 
 	unsigned width = 256, height = 256;
 	std::vector<unsigned char> image;
@@ -34,13 +42,24 @@ int main() {
 		for (unsigned y = 0; y < height; y++) {
 			int index = 4 * width * y + 4 * x;
 			
-			float c = (hit_sphere(sphereCenter, sphereRadius, Vector3(x, y, 0), Vector3(0, 0, 1)) >= 0) ? 0 : 255;
+			if (hit_sphere(sphereCenter, sphereRadius, Vector3(x, y, 0), Vector3(0, 0, 1)) >= 0) {
+				Vector3 rayOrigin = Vector3(x, y, 0);
 
-			image[index] = c;		// Red
-			image[index + 1] = c;	// Green
-			image[index + 2] = c;	// Blue
-			image[index + 3] = 255; // Alpha
+				float c = (hit_sphere(sphereCenter, sphereRadius, rayOrigin, (lightPosition - rayOrigin).normalized()) >= 0) ? 255 : 0;
 
+				//setColor(image, index, c, c, c, 255);
+				image[index] = c;		// Red
+				image[index + 1] = c;	// Green
+				image[index + 2] = c;	// Blue
+				image[index + 3] = 255; // Alpha
+			}
+			else {
+				//setColor(image*, index, 255, 255, 255, 255);
+				image[index] =	   255;	// Red
+				image[index + 1] = 255;	// Green
+				image[index + 2] = 255;	// Blue
+				image[index + 3] = 255; // Alpha
+			}
 		}
 	}
 
--- lib/vector.h
+++ lib/vector.h
@@ -1,5 +1,6 @@
 #ifndef VECTOR_H
 #define VECTOR_H
+using namespace std;
 
 class Vector3 {
 public:
@@ -11,11 +12,30 @@ public:
 	Vector3(float vectorX, float vectorY, float vectorZ);
 	float lengthSquare();
 	float length();
-	static float dot(Vector3 v1, float f);
+	static float dot(Vector3 v1, float f) {
+		return v1.x * f + v1.y * f + v1.z * f;
+	}
 	float dot(float f);
-	static Vector3 cross(Vector3 v1, Vector3 v2);
+	static float dot(Vector3 v1, Vector3 v2) {
+		return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+	}
+	float dot(Vector3 v2);
+	static Vector3 cross(Vector3 v1, Vector3 v2) {
+		Vector3 vector;
+		vector.x = v1.y * v2.z - v1.z * v2.y;
+		vector.y = v1.z * v2.x - v1.x * v2.z;
+		vector.z = v1.x * v2.y - v1.y * v2.x;
+		return vector;
+	}
 	Vector3 cross(Vector3 v);
-	static Vector3 normalize(Vector3 v1);
+	static Vector3 normalize(Vector3 v1) {
+		try {
+			return v1 / v1.length();
+		}
+		catch (string err) {
+			cout << err + "\n";
+		}
+	}
 	Vector3 normalized();
 	Vector3 operator+(const Vector3 v);
 	Vector3 operator+(float f);
@@ -24,7 +44,10 @@ public:
 	Vector3 operator/(const float f);
 	Vector3 operator*(const float f);
 	Vector3 operator*(const Vector3 v);
-	friend std::ostream &operator<<(std::ostream &output, const Vector3 &vector);
+	friend std::ostream& operator<<(std::ostream& output, const Vector3& vector) {
+		output << "(" << vector.x << " ; " << vector.y << " ; " << vector.z << ")";
+		return output;
+	}
 };
 
 #endif
