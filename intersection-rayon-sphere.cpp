184bb8650cefb8425886cdba378661fdd1818c00
Valentin Lamprière <valentin.lampriere1@gmail.com>
2020-10-06 10:43:05 +0200

    Reduce ray offset

--- intersection-rayon-sphere.cpp
+++ intersection-rayon-sphere.cpp
@@ -125,7 +125,7 @@ Color manageLightReflection(Vector3 pointOrigin, Vector3 pointIntersection, Sphe
 	Color colXY = Color(0, 0, 0);
 
 	if (s.isMirror == false) {
-		int nbRay = 20;
+		int nbRay = 10;
 		for (Light& aLight : lightsSources) {
 
 			Color colorALight = Color();
@@ -174,23 +174,21 @@ int main() {
 	spheres.push_back(Sphere(Vector3(800, 350, 300), 80, Color(255, 255, 0)));
 
 	// ADD LIGHTS
-	lightsSources.push_back(Light(Vector3(200, -200, 400), Color(255, 255, 255), 4000000, 200.0f));
-	//lightsSources.push_back(Light(Vector3(1250, 500, 150), Color(0, 200, 255), 800000));
-	lightsSources.push_back(Light(Vector3(-100, 300, -150), Color(240, 80, 0), 1500000, 1.0f));
+	lightsSources.push_back(Light(Vector3(200, -200, 400), Color(255, 255, 255), 2000000, 200.0f));
+	lightsSources.push_back(Light(Vector3(-100, 300, -150), Color(240, 80, 0), 750000, 1.0f));
 
 	image.resize(camera.width * camera.height * 4);
 
 	for (unsigned x = 0; x < camera.width; x++) {
 		for (unsigned y = 0; y < camera.height; y++) {
 			int index = 4 * camera.width * y + 4 * x;
-			int nbRay = 200;
+			int nbRay = 100;
 			Color colXY = Color(0, 0, 0);
 
 			for (int i = 0; i < nbRay; i++) {
-				Vector3 offset = Vector3(random(), random(), random());
+				Vector3 offset = Vector3(random() * 0.01f, random() * 0.01f, random() * 0.01f);
 				Vector3 point = Vector3(camera.position.x + x, camera.position.y + y, camera.position.z);
 
-				//Ray r = Ray(point, camera.GetNormalAtPoint(point));
 				Ray r = Ray(point + offset, (point + offset - camera.origin).normalized());
 
 				float distanceFirstSphere;
